ad8f3fc27397209d62ad46788b0b34942718e6b6
Salvador Tamarit <stamarit@software.imdea.org>
2018-07-31 11:19:43 +0200

    url-hashtag-segmentation [IMPROVED]
    https://www.hackerrank.com/challenges/url-hashtag-segmentation

--- url-hashtag-segmentation/solution.py
+++ url-hashtag-segmentation/solution.py
@@ -4,7 +4,7 @@ import sys
 
 def clean_url(line0):
 	i = 0
-	line = line0[4:] if line0[0] == 'w' else line0
+	line = line0[4:] if line0[0:4] == "www." else line0
 	rline = list(reversed(line))
 	last_dot = 0
 	while i < len(line):
@@ -37,50 +37,90 @@ def get_words():
 			words_content.split("\n")))
 	words = {}
 	for i in range(0, len(words_lines)):
-	    words[words_lines[i]] = 0
+	    words[words_lines[i].strip().lower()] = 0
 	# print(words)
 	return words
 
-def separate(lines, words):
-	total_words = []
-	for line in lines:
-		terms = []
-		j = 1
-		current_word_start = 0
-		last_ok = 0
-		while j <= len(line):
-			current_word = line[current_word_start:j]
-			# print(current_word)
-			if current_word in words:
-				# print("IN WORDS")
-				last_ok = j
-			else:
-				if last_ok != 0:
-					terms.append(line[current_word_start:last_ok])
-					current_word_start = j - 1
-					last_ok = 0
-					j-=1
-			j+=1
-		if last_ok != 0:
-			terms.append(line[current_word_start:last_ok])
-		if terms == [] or last_ok != len(line) :
-			print(line)
-			total_words.append([line])
-		else :
-			print(" ".join(terms))
-			total_words.append(terms)
-	return total_words
+# def separate(lines, words):
+# 	total_words = []
+# 	for line in lines:
+# 		terms = []
+# 		j = 1
+# 		current_word_start = 0
+# 		last_ok = 0
+# 		while j <= len(line):
+# 			current_word = line[current_word_start:j]
+# 			# print(current_word)
+# 			if current_word in words:
+# 				# print("IN WORDS")
+# 				last_ok = j
+# 				last_ok_serie.append((current_word_start, j))
+# 			elif last_ok != 0 :
+# 					terms.append(line[current_word_start:last_ok])
+# 					current_word_start = j - 1
+# 					last_ok = 0
+# 					j-=1
+# 			j+=1
+# 		if last_ok != 0:
+# 			terms.append(line[current_word_start:last_ok])
+# 		if terms == [] or last_ok != len(line) :
+# 			print(line)
+# 			total_words.append([line])
+# 		else :
+# 			print(" ".join(terms))
+# 			total_words.append(terms)
+# 	return total_words
+
+def is_number(num_str):
+    try:
+    	int(num_str)
+    	return True
+    except ValueError:
+        try:
+        	float(num_str)
+        	return True
+        except ValueError:
+        	return False
+
+def process_line(stack, line, words, current_word_start, j, consolidated):
+	if j > len(line):
+		if stack == []:
+			return [(0, len(line))]
+		else:
+			(last_current_word_start, last_j, last_consolidated0) = stack.pop()
+			last_consolidated = last_consolidated0.copy()
+			last_consolidated.append((last_current_word_start, last_j))
+			if last_j == len(line):
+				return last_consolidated
+			else: 
+				return process_line(stack, line, words, last_j, last_j + 1, last_consolidated)
+	else:
+		current_word = line[current_word_start:j]
+		if current_word in words:
+			stack.append((current_word_start, j, consolidated))
+		elif is_number(current_word):
+			stack.append((current_word_start, j, consolidated))
+		return process_line(stack, line, words, current_word_start, j + 1, consolidated)
+
+
             
+def separate(lines, words):
+	for line in lines:
+		line_words = process_line([], line, words, 0, 1, [])
+		line_words_print = []
+		for (start, end) in line_words:
+			line_words_print.append(line[start:end])
+		print(" ".join(line_words_print))
 
 if __name__ == '__main__':
 	words = get_words()
-	s = sys.stdin.read()[1:]
+	s = sys.stdin.read()
 	lines = list(
     	filter(
 			lambda x: x != " " and x != "",
 			s.split("\n")))
-	cleaned_lines = clean(lines)
+	cleaned_lines = clean(lines[1:])
 	# print(cleaned_lines)
-	separated = separate(cleaned_lines, words)
+	separate(cleaned_lines, words)
 	# print(separated)
 
